75e9a7670c6dbb580bd0a90ec47162c8609a8bdb
fubira <fubira@gmail.com>
2019-06-24 17:02:56 +0900

    server base

---
+++ server/reset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -f ./lobby/playerdata/*.dat
+rm -f ./plugins/dynmap/hiddenplayers.txt
+rm -f ./plugins/Skript/variables.csv
+rm -f ./plugins/Skript/cache/*
+rm -f ./plugins/SimpleFly/player.list
+rm -f ./plugins/SpectatorPlus/spectators.yml
+rm -f ./plugins/SpectatorPlus/specs.yml
+rm -f ./plugins/Spawn_Bed/config.yml
+rm -f ./plugins/TreeAssist/data.yml
+
