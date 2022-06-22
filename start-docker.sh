1fae851cdd51371963835955f746d8f2753cc078
fubira <fubira@gmail.com>
2022-06-23 00:04:11 +0900

    spigot 1.18.1

--- start-docker.sh
+++ start-docker.sh
@@ -2,13 +2,13 @@
 echo Run minecraft-survival-server on Docker
 docker run -d -it \
   -v /mnt/storage/backup:/backup \
-  -v /data/minecraft/survival-1.16/server:/data \
+  -v /data/minecraft/survival/server:/data \
   -v /etc/timezone:/etc/timezone:ro \
   -v /etc/localtime:/etc/localtime:ro \
-  -e 'JVM_OPTS=-Xmx1024M -Xms1024M' \
+  -e 'JVM_OPTS=-Xmx4096M -Xms4096M' \
   -e EULA=TRUE \
   -e TYPE=PAPER \
-  -e VERSION=1.16.5 \
+  -e VERSION=1.18.1 \
   -p 25566:25565 \
   -p 8124:8123 \
   --name survival \
